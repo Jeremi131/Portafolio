21466040f357209db35e5c5b7f34fb39dd7f82e3
Jeremi131 <jeremijared10@gmail.com>
2023-01-06 19:35:13 -0400

    four commit

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const iconMenu = document.querySelector("#color");
-const menu = document.querySelector(".menu_nav");
+const menu = document.querySelector("#menu_nav");
+const cerrarHome = document.querySelector("#close_home")
+const cerrarExperience = document.querySelector("#close_experience")
+const cerrarPortafolio = document.querySelector("#close_portafolio")
+const cerrarContacto = document.querySelector("#close_contacto")
+
 
 
 iconMenu.addEventListener('click', function () {
-    menu.classList.toggle("menu_show")
+    if (menu.className == "menu_nav") {
+        menu.className = "menu_show";
+    } else {
+        menu.className = "menu_nav";
+    }
 
     if (iconMenu.className == "icon_menu") {
         iconMenu.className = "change-color";
@@ -11,4 +20,44 @@ iconMenu.addEventListener('click', function () {
         iconMenu.className = "icon_menu";
     }
 });
+
+cerrarHome.addEventListener('click', function () {
+    if (menu.className == "menu_show") {
+        menu.className = "menu_nav";
+    } 
+
+    if (iconMenu.className == "change-color") {
+        iconMenu.className = "icon_menu";
+    }
+})
+
+cerrarExperience.addEventListener('click', function () {
+    if (menu.className == "menu_show") {
+        menu.className = "menu_nav";
+    } 
+
+    if (iconMenu.className == "change-color") {
+        iconMenu.className = "icon_menu";
+    }
+})
+
+cerrarPortafolio.addEventListener('click', function () {
+    if (menu.className == "menu_show") {
+        menu.className = "menu_nav";
+    } 
+
+    if (iconMenu.className == "change-color") {
+        iconMenu.className = "icon_menu";
+    }
+})
+
+cerrarContacto.addEventListener('click', function () {
+    if (menu.className == "menu_show") {
+        menu.className = "menu_nav";
+    } 
+
+    if (iconMenu.className == "change-color") {
+        iconMenu.className = "icon_menu";
+    }
+})
 
